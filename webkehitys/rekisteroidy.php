189db26330d468f18ae8ecc7c10e73cce325ec68
HeidiN86 <heidi.nuust@gmail.com>
2023-02-15 19:38:14 +0200

    yhdistäminen ja ylimääräisen poisto

--- webkehitys/rekisteroidy.php
+++ webkehitys/rekisteroidy.php
@@ -3,19 +3,22 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
     isset($_POST["etunimi"]) && isset($_POST["sukunimi"])){
     $tunnus=$_POST["tunnus"];
     $salasana=$_POST["salasana"];
+    //$salasana2=$_POST["salasana2"]; //tarvitaanko tätä riviä? tai haittaako se, kun ei ole tietokannassa salasana2 eikä sitä sinne haluta?
     $etunimi=$_POST["etunimi"];
     $sukunimi=$_POST["sukunimi"];
+    if($salasana != $salasana2) die('Passwords do not match!');
 }
 else{
     header("Location:rekisteroityminen.html");
+    // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
 }
 
 $yhteys=mysqli_connect("db", "erika", "projekti");
-$tietokanta=mysqli_select_db($yhteys, "poj_userdata");
-$sql="insert into poj_users values(?, md5(?), ?, ?)";
+$tietokanta=mysqli_select_db($yhteys, "reseptikanta");
+$sql="insert into kayttaja values(?, md5(?), ?, ?)";
 $stmt=mysqli_prepare($yhteys, $sql);
-mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi);
+mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); //???????jäjestyksellä väliä, täällä vs ylhäällä isset?
 mysqli_stmt_execute($stmt);
 
 header("Location:kiitos.html");
