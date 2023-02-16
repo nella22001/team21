961613f7590fdb5537fe80fc40627374f1edd1d3
HeidiN86 <heidi.nuust@gmail.com>
2023-02-16 21:23:29 +0200

    rek. toimimaan?

--- webkehitys/rekisteroidy.php
+++ webkehitys/rekisteroidy.php
@@ -9,19 +9,19 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
     if($salasana != $salasana2) die('Passwords do not match!');
 }
 else{
-    header("Location:rekisteroityminen.html");
+    header("Location:./webkehitys/rekisteroityminen.html");
     // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
 }
 
-$yhteys=mysqli_connect("db", "erika", "projekti");
+$yhteys=mysqli_connect("db", "root", "password");
 $tietokanta=mysqli_select_db($yhteys, "reseptikanta");
 $sql="insert into kayttaja values(?, md5(?), ?, ?)";
 $stmt=mysqli_prepare($yhteys, $sql);
 mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); //???????jäjestyksellä väliä, täällä vs ylhäällä isset?
 mysqli_stmt_execute($stmt);
 
-header("Location:kiitos.html");
+header("Location:./webkehitys/kiitos.html");
 exit;
 
 ?>
