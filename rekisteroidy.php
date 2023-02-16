f58a21bb867e75b93877d1cfec6dc9eb50867c8c
HeidiN86 <heidi.nuust@gmail.com>
2023-02-16 22:12:06 +0200

    rek.ongelma test 5

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -9,7 +9,7 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
     if($salasana != $salasana2) die('Passwords do not match!');
 }
 else{
-    header("Location:https://nella22001.github.io/team21/webkehitys/rekisteroityminen.html");
+    header("Location:https://nella22001.github.io/team21/rekisteroityminen.html");
     // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
 }
@@ -21,7 +21,7 @@ $stmt=mysqli_prepare($yhteys, $sql);
 mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); //???????jäjestyksellä väliä, täällä vs ylhäällä isset?
 mysqli_stmt_execute($stmt);
 
-header("Location:https://nella22001.github.io/team21/webkehitys/kiitos.html");
+header("Location:https://nella22001.github.io/team21/kiitos.html");
 exit;
 
 ?>
