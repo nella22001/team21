f8f50dfad0f8ede2696a58c438b688642154e996
HeidiN86 <heidi.nuust@gmail.com>
2023-02-16 21:52:10 +0200

    rek. ongelma test

--- webkehitys/rekisteroidy.php
+++ webkehitys/rekisteroidy.php
@@ -6,10 +6,10 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
     //$salasana2=$_POST["salasana2"]; //tarvitaanko tätä riviä? tai haittaako se, kun ei ole tietokannassa salasana2 eikä sitä sinne haluta?
     $etunimi=$_POST["etunimi"];
     $sukunimi=$_POST["sukunimi"];
-    //if($salasana != $salasana2) die('Passwords do not match!');
+    if($salasana != $salasana2) die('Passwords do not match!');
 }
 else{
-    header("Location:./webkehitys/rekisteroityminen.html");
+    header("Location:rekisteroityminen.html");
     // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
 }
@@ -21,7 +21,7 @@ $stmt=mysqli_prepare($yhteys, $sql);
 mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); //???????jäjestyksellä väliä, täällä vs ylhäällä isset?
 mysqli_stmt_execute($stmt);
 
-header("Location:./webkehitys/kiitos.html");
+header("Location:kiitos.html");
 exit;
 
 ?>
