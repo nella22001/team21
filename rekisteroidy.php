112eda842f89b0f2ca391a4cf072680083b92439
HeidiN86 <heidi.nuust@gmail.com>
2023-02-17 04:24:49 +0200

    rek php jos lauseita

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -6,22 +6,31 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
     //$salasana2=$_POST["salasana2"]; //tarvitaanko tätä riviä? tai haittaako se, kun ei ole tietokannassa salasana2 eikä sitä sinne haluta?
     $etunimi=$_POST["etunimi"];
     $sukunimi=$_POST["sukunimi"];
-    if($salasana != $salasana2) die('Passwords do not match!');
+    if($salasana != $salasana2) {
+        die('Passwords do not match!');
+        exit;
 }
 else{
-    header("Location:https://nella22001.github.io/team21/rekisteroityminen.html");
+    header("Location:rekisteroityminen.html");
     // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
 }
 
 $yhteys=mysqli_connect("db", "erika", "projekti");
+if (!$yhteys) {
+    die ("Failed to make a connection: " . mysqli_connect_error());
+}
 $tietokanta=mysqli_select_db($yhteys, "reseptikanta");
+if (!$tietokanta) {
+    die ("Failed to connect to the right database: " . mysqli_connect_error());
+}
 $sql="insert into kayttaja values(?, md5(?), ?, ?)";
 $stmt=mysqli_prepare($yhteys, $sql);
 mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); //???????jäjestyksellä väliä, täällä vs ylhäällä isset?
 mysqli_stmt_execute($stmt);
+mysqli_close($yhteys);
 
-header("Location:https://nella22001.github.io/team21/kiitos.html");
+header("Location:kiitos.html");
 exit;
 
 ?>
