c966b0abd06d2a615e0aa1ae1a39f2b1df3aea41
erika-i-m <112096701+erika-i-m@users.noreply.github.com>
2023-02-17 10:58:01 +0200

    .

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -14,7 +14,7 @@ else{
     header("Location:rekisteroityminen.html");
     // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
-}
+  }
 }
 $yhteys=mysqli_connect("db", "erika", "projekti");
 if (!$yhteys) {
