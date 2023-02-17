1cb0e18f686ac52dab83f5dc31cec5def229133c
erika-i-m <112096701+erika-i-m@users.noreply.github.com>
2023-02-17 12:11:25 +0200

    penan edits

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -9,14 +9,15 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
     if($salasana != $salasana2) {
         die('Passwords do not match!');
         exit;
+    }
 }
 else{
-    header("Location:rekisteroityminen.html");
+    print "Tadaa";
+    // header("Location:rekisteroityminen.html");
     // tai tulosta print "Sorry, some information is missing or the username $tunnus is already registered. Please try again." Pitää vielä testata
     exit;
-  }
 }
-$yhteys=mysqli_connect("db", "erika", "projekti");
+$yhteys=mysqli_connect("db", "root", "password");
 if (!$yhteys) {
     die ("Failed to make a connection: " . mysqli_connect_error());
 }
@@ -24,7 +25,10 @@ $tietokanta=mysqli_select_db($yhteys, "reseptikanta");
 if (!$tietokanta) {
     die ("Failed to connect to the right database: " . mysqli_connect_error());
 }
-$sql="insert into kayttaja values(?, md5(?), ?, ?)";
+
+// print "$tunnus, $salasana, $etunimi, $sukunimi";
+
+$sql="insert into kayttaja(tunnus, salasana, etunimi, sukunimi) values(?, md5(?), ?, ?)";
 $stmt=mysqli_prepare($yhteys, $sql);
 mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); //???????jäjestyksellä väliä, täällä vs ylhäällä isset?
 mysqli_stmt_execute($stmt);
