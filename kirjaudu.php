d1fc610565cf5350ce916905c3ec21e1634f30c5
helmi22007 <113498279+helmi22007@users.noreply.github.com>
2023-02-17 09:05:14 +0200

    add

---
+++ kirjaudu.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Log in</title>
+</head>
+<body>
+    <style>
+        h2{text-align: center;
+        margin-top: 250px;}
+        form{text-align: center;}
+        p{margin-bottom: 250px;}
+       
+    
+    </style>
+    <?php
+   
+    print "<h2>Log in</h2>";
+    ?>
+    
+    <form action='tarkistakirjautuminen.php' method='post'>
+    Username: <input type='text' name='tunnus' value=''><br><br>
+    Password: <input type='password' name='salasana' value=''><br><br>   
+    <input type='submit' name='ok' value='Log in'><br><br>
+    <p>You do not have account? Create account <a href=rekisteroityminen.html>here</a></p>
+   
+</form>  
+   
+</body>
+</html>
