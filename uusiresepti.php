46815401be81c0cfaf9d288dac81f1611ea2bb81
HeidiN86 <heidi.nuust@gmail.com>
2023-02-21 11:28:34 +0200

    uusiresepti kuntoon

--- uusiresepti.php
+++ uusiresepti.php
@@ -1,4 +1,7 @@
 <?php
+
+print "Tadaa";
+exit;
 session_start();
 //tutkitaan, onko olemassa käynnissäolevaa kirjautumista
 if (!isset($_SESSION["user_ok"])){ //jos sessioniin ei ole laitettu sellaista user ok arvoa, käyttäjä ei ole kirjautunut
@@ -11,8 +14,8 @@ if (!isset($_SESSION["user_ok"])){ //jos sessioniin ei ole laitettu sellaista us
 
 $json=isset($_POST["reseptit"]) ? $_POST["reseptit"] : "";
 
-if (!($reseptit=tarkistaJson($json))){
-    print "Fill out all the fields, some information is missing!";
+if (!($reseptit=tarkistaJson($json))){ //jos ehdot täyttyy, tulee olio, mikä palautetaan
+    print "Fill out all the fields, some information is missing!"; // jos tulee false
     exit;
 }
 
@@ -25,21 +28,22 @@ catch(Exception $e){
     print "Yhteysvirhe!";
     exit;
 }
-if (isset($reseptit->nimi) && $reseptit->nimi>0) { //tehdään päivitys
-    $sql="update reseptit set ainekset=?, ohje=? where nimi=?";
+// if (isset($reseptit->nimi) && !empty($reseptit->nimi)) { //tehdään päivitys
+//     $sql="update reseptit set ainekset=?, ohje=? where nimi=?";
+//     $stmt=mysqli_prepare($yhteys, $sql);
+//     mysqli_stmt_bind_param($stmt, 'sss', $reseptit->ainekset, $reseptit->ohje, $reseptit->nimi);
+// } 
+// else { //tai tehdään lisäys
+    //Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
+    //joihin laitetaan muuttujien arvoja
+    $sql="insert into reseptit (nimi, ainekset, ohje) values(?, ?, ?)";
+    //Valmistellaan sql-lause
     $stmt=mysqli_prepare($yhteys, $sql);
-    mysqli_stmt_bind_param($stmt, 'sss', $reseptit->ohje, $reseptit->ainekset, $reseptit->nimi);
-} else { //tai tehdään lisäys
-//Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
-//joihin laitetaan muuttujien arvoja
-$sql="insert into reseptit (nimi, ainekset, ohje) values(?, ?, ?)";
-//Valmistellaan sql-lause
-$stmt=mysqli_prepare($yhteys, $sql);
-//Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'sss', $reseptit->nimi, $reseptit->ainekset, $reseptit->ohje);
-}
-//Suoritetaan sql-lause
-mysqli_stmt_execute($stmt);
+    //Sijoitetaan muuttujat oikeisiin paikkoihin
+    mysqli_stmt_bind_param($stmt, 'sss', $reseptit->nimi, $reseptit->ainekset, $reseptit->ohje);
+    //Suoritetaan sql-lause
+    mysqli_stmt_execute($stmt);
+// }
 //Suljetaan tietokantayhteys
 mysqli_close($yhteys);
 print "Paluupostina jsonia ".$json;
@@ -55,6 +59,6 @@ function tarkistaJson($json){
     if (empty($reseptit->nimi) || empty($reseptit->ainekset) || empty($reseptit->ohje) ){
         return false;
     }
-    return $respetit;
+    return $reseptit;
 }
 ?>
