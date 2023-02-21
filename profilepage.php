df2819b72ff8a93fe35cded517e9d7558d5c5354
HeidiN86 <heidi.nuust@gmail.com>
2023-02-22 00:09:39 +0200

    profile page links into buttons

--- profilepage.php
+++ profilepage.php
@@ -64,9 +64,9 @@ if (!$tietokanta) {
                  </ul>
                  </div>
                  <div class="sidenav">
-                <a href="#">Add a recipe</a>
-                <a href="#">Change recipes</a>
-                <a href="#">Delete recipes</a>
+                <a href="lisaaresepti.php"><button class="button">Add a recipe</button></a>
+                <a href="#"><button class="button">Change recipes</button></a>
+                <a href="#"><button class="button">Delete recipes</button></a>
               </div>
          </nav>
          </header>
@@ -119,6 +119,7 @@ if (!$tietokanta) {
 mysql_close($yhteys); // Closing Connection with Server
 ?>
 
+<a href="kirjauduulos.php">Sign out</a>
         </div><!--background ends-->
 
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
