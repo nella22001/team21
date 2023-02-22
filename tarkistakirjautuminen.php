3c5d6b401ad02930fca6694e4e4806208611f641
helmi22007 <113498279+helmi22007@users.noreply.github.com>
2023-02-22 09:39:52 +0200

    change html --> php

--- tarkistakirjautuminen.php
+++ tarkistakirjautuminen.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); //session start lause mahdollistaa session olion käyttämisen
 if (!isset($_SESSION["paluuosoite"])){
-    $_SESSION["paluuosoite"]="profilepage.html"; //ei ole paluuosoitetta kun vasta tultiin
+    $_SESSION["paluuosoite"]="profilepage.php"; //ei ole paluuosoitetta kun vasta tultiin
 }
 if (isset($_POST["tunnus"]) && isset($_POST["salasana"])){
     $tunnus=$_POST["tunnus"];
