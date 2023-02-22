3ccdbc97e64aea1da04a336aee442e7d5d54d215
erika-i-m <112096701+erika-i-m@users.noreply.github.com>
2023-02-22 14:55:54 +0200

    edits

--- profilepage.php
+++ profilepage.php
@@ -84,9 +84,9 @@ if (!isset($_SESSION["user_ok"])){ //jos sessioniin ei ole laitettu sellaista us
                                                     die ("Failed to connect to the right database: " . mysqli_connect_error());
                                                 }
                                                 $tulos=mysqli_query($yhteys, "select * from kayttaja");
-                                                 while ($rivi=mysqli_fetch_object($tulos)){
-                                                 print "<tr><td>$rivi->tunnus";
-                                                 }
+                                                while ($rivi=mysqli_fetch_object($tulos)){
+                                                 print "<tr><td>$rivi->tunnus\n<br>";
+                                                }
                                                 
                                                  
                         ?>
