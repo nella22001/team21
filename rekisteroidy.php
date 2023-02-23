541092f83710b3ab8b3261479068e38a21f26c60
erika-i-m <112096701+erika-i-m@users.noreply.github.com>
2023-02-23 13:44:00 +0200

    edits

--- rekisteroidy.php
+++ rekisteroidy.php
@@ -11,53 +11,52 @@ if (isset($_POST["tunnus"]) && isset($_POST["salasana"]) &&
 
     //Erroreiden tarkistusta ehtolauseilla.
     //Takastetaan täsmääkö salasanat toisiinsa ja jos jokin kohta on jäänyt tyhjäksi niin myös ne kohdat tarkastetaan.
-    //Rekisteröiti ei mene läpi jos kohtia on tyhjänä tai salasanat ei täsmää. 
+    //Rekisteröiti ei mene läpi jos kohtia on tyhjänä tai salasanat eivät täsmää. 
     if($salasana != $salasana2) {
         die('Passwords do not match! Please try again!');
     }
     elseif(empty($etunimi)){
-        die('Please fill out your first name.');
+        die('Please fill out your first name and try again.');
     }
     elseif(empty($sukunimi)){
-        die('Please fill out your last name.');
+        die('Please fill out your last name and try again.');
     }
     elseif(empty($tunnus)){
-        die('Please fill out your username.');
+        die('Please fill out your username and try again.');
     }
     elseif(empty($salasana || $salasana2)){
         die('The password field is empty. Please try again!');
     }  
  }
-
     else{
         header("Location:rekisteroityminen.html");
      exit;
+ }
+
+    //Otetaan yhteys tietokantaan. Palvelimena db, joka on dockeriin asennettu tietokanta. Tunnus erika ja salasana on projekti. 
+    //Jos ongelmia yhteydessä tai tietokantaan yhdistymisessä niin ilmoitetaan siitä. 
+    $yhteys=mysqli_connect("db", "erika", "projekti");
+    if (!$yhteys) {
+        die ("Failed to create a connection: " . mysqli_connect_error());
     }
-//Otetaan yhteys tietokantaan. Palvelimena db, joka on dockeriin asennettu tietokanta. Tunnus erika ja salasana on projekti. 
-$yhteys=mysqli_connect("db", "erika", "projekti");
-if (!$yhteys) {
-    die ("Failed to create a connection: " . mysqli_connect_error());
-}
-$tietokanta=mysqli_select_db($yhteys, "reseptikanta");
-if (!$tietokanta) {
-    die ("Failed to connect to the right database: " . mysqli_connect_error());
-}
+    $tietokanta=mysqli_select_db($yhteys, "reseptikanta");
+    if (!$tietokanta) {
+        die ("Failed to connect to the right database: " . mysqli_connect_error());
+    }
 
-//Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
-//joihin laitetaan muuttujien arvoja
-//Sijoitetaan muuttujat sql-lauseeseen
-$sql="insert into kayttaja(tunnus, salasana, etunimi, sukunimi) values(?, md5(?), ?, ?)";
-//Valmistellaan sql-lause.
-$stmt=mysqli_prepare($yhteys, $sql);
-//Sijoitetaan muuttujat oikeisiin paikkoihin. s=merkkijono. 
-mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); 
-//Suljetaan tietokantayhteys.
-mysqli_close($yhteys);
+    //Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat joihin laitetaan muuttujien arvoja
+    $sql="insert into kayttaja(tunnus, salasana, etunimi, sukunimi) values(?, md5(?), ?, ?)";
+    //Valmistellaan sql-lause.
+    $stmt=mysqli_prepare($yhteys, $sql);
+    //Sijoitetaan muuttujat oikeisiin paikkoihin. s tarkoittaamerkkijonoa. 
+    mysqli_stmt_bind_param($stmt, "ssss", $tunnus, $salasana, $etunimi, $sukunimi); 
+    //Suljetaan tietokantayhteys.
+    mysqli_close($yhteys);
 
-//Rekisteröinti tehty onnistuneesti.
-//Ohjataan käyttäjä kiitos.html sivustolle.
-//Sivustolla käyttäjä saa ilmoituksen onnistuneesta rekisteröinnista.
-//Käyttäjä pääsee linkkiä painamalla kirjautumaan ja sitä kautta omalle profiili sivustolleen painamalla linkkiä.
-header("Location:kiitos.html");
-exit;
+    //Rekisteröinti tehty onnistuneesti.
+    //Ohjataan käyttäjä kiitos.html sivustolle.
+    //Sivustolla käyttäjä saa ilmoituksen onnistuneesta rekisteröinnista.
+    //Käyttäjä pääsee linkkiä painamalla kirjautumaan ja sitä kautta omalle profiili sivustolleen painamalla linkkiä.
+    header("Location:kiitos.html");
+    exit;
 ?>
